ff018d40c0800ac8d6ff15db03528fdc8e2c7372
Ghostkeeper <ghost_keeper+github@hotmail.com>
2015-11-17 14:26:31 +0100

    Fix segfault
    The construct of `bool picked[n] = {false}` gave a segfault for some reason, even though I just copied it from the internet where they used the same construct with an int array. Maybe this doesn't work properly for booleans with this construct in GCC.
    
    Contributes to issue CURA-329.

--- src/pathOrderOptimizer.cpp
+++ src/pathOrderOptimizer.cpp
@@ -343,7 +343,8 @@ std::vector<std::vector<size_t>> LineOrderOptimizer::cluster()
         grid.insert(polygons[polygon_index][polygons[polygon_index].size() - 1],polygon_index);
     }
     std::vector<std::vector<size_t>> clusters;
-    bool picked[polygons.size()] = {false}; //For each polygon, whether it is already picked.
+    bool picked[polygons.size()]; //For each polygon, whether it is already picked.
+    memset(picked,0,polygons.size()); //Initialise to false.
     for(size_t polygon_index = 0;polygon_index < polygons.size();polygon_index++) //Then greedily find all clusters.
     {
         if(picked[polygon_index]) //Already in a cluster.
