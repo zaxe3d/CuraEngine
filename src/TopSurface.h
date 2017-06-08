c764db5e4d2a162f8510be015f4e75ec49a7d5a2
Ghostkeeper <rubend@tutanota.com>
2017-06-08 17:18:36 +0200

    Document sandBelow
    Contributes to issue CURA-3903.

--- src/TopSurface.h
+++ src/TopSurface.h
@@ -54,6 +54,22 @@ public:
      */
     bool sand(const SliceMeshStorage& mesh, const GCodePathConfig& line_config, LayerPlan& layer);
 
+    /*!
+     * \brief Generate paths for sanding between a lower layer and this one.
+     *
+     * This draws a lot of lines diagonally moving from a lower layer to this
+     * layer. These lines start on points along the perimeter of the lower layer
+     * and take the shortest route towards this layer. This technique should
+     * cover most of the area between the two layers. It may also miss some
+     * areas that are not directly between the two layers and it may go outside
+     * of the polygon of the lower layer. TODO: Prevent this.
+     *
+     * \param mesh The settings base to get our sanding settings from.
+     * \param line_config The configuration of the sanding lines to use. Note
+     * that the flow might still get adjusted by the sanding settings.
+     * \param top_surface_below The top surface of the layer below this layer.
+     * \param[out] layer The output g-code layer to put the resulting lines in.
+     */
     bool sandBelow(const SliceMeshStorage& mesh, const GCodePathConfig& line_config, const TopSurface& top_surface_below, LayerPlan& layer);
 };
 
